33bea4a035f8b835021bd2ce405029b99d4eb2de
Shibumishra <sibumishra487@gmail.com>
2021-06-28 23:15:33 +0530

    first commit

---
+++ src/components/PageNotFound/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const PageNotFound = () => {
+    return (<>
+        
+	<section id="not-found">
+    <div class="circles">
+      <p>404<br />
+       <small>PAGE NOT FOUND</small>
+      </p>
+      <span class="circle big"></span>
+      <span class="circle med"></span>
+      <span class="circle small"></span>
+    </div>
+  </section>
+    </>);
+}
+
+export default PageNotFound;
